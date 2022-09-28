d763578d5ee10da6bb85c3cbbac04d8d66629ecd
Franciscoborges2002 <franciscomsborges2002@gmail.com>
2022-09-28 22:52:54 +0100

    Progresses in day 3

--- 2015/Day03/03.js
+++ 2015/Day03/03.js
@@ -5,7 +5,7 @@ const zeroThree = function(input){
     let allMovesDict = {};
     var totalPresents = 1, moreThanOne = 0;
     var numeroValoresVerificarX = 1, numeroValoresVerificarY = 1;
-    var lastCoord = "00";
+    var lastCoord = "00", lastLastCoord, partOne, partTwo;
 
     allMovesDict[lastCoord] = 1;
 
@@ -13,10 +13,22 @@ const zeroThree = function(input){
         //console.log(lastCoord)
         switch(input[i]){
             case '^':
+                lastLastCoord = lastCoord;
+                
                 lastCoord = lastCoord.substring(0,numeroValoresVerificarX) + (parseInt(lastCoord.substring(numeroValoresVerificarX,numeroValoresVerificarX + numeroValoresVerificarY),10) + 1).toString();
+                console.log("^ " + lastCoord + "(" + lastCoord.substring(0,numeroValoresVerificarX) + " " + lastCoord.substring(numeroValoresVerificarX,numeroValoresVerificarX + numeroValoresVerificarY) +")//" + "nX: " + numeroValoresVerificarX + " ,nY: " + numeroValoresVerificarY);
 
-                if((lastCoord.substring(numeroValoresVerificarX,numeroValoresVerificarX + numeroValoresVerificarY),10) == 10){
-                    numeroValoresVerificarY = 2;
+                if(lastCoord.substring(numeroValoresVerificarX,numeroValoresVerificarX + 1) === '-'){//If Y coord goes negative
+                    if((lastCoord.substring(numeroValoresVerificarX,numeroValoresVerificarX + numeroValoresVerificarY),10) < -9){//If Y coord passes 10 negatives
+                        numeroValoresVerificarY = 3;
+                    }
+                }else{//if its positive
+                    if(lastCoord.length === 2 && lastLastCoord.substring(0, 1) === '-'){//And last had a -
+                        numeroValoresVerificarY = 1;
+                    }
+                    if(lastCoord.substring(numeroValoresVerificarX,numeroValoresVerificarX + numeroValoresVerificarY) > 9){//If Y coord passes 10
+                        numeroValoresVerificarY = 2;
+                    }
                 }
 
                 if(allMovesDict[lastCoord] === undefined){
@@ -27,7 +39,29 @@ const zeroThree = function(input){
                 
             break;
             case '>':
-                lastCoord = (parseInt(lastCoord.substring(0,1),10) + 1).toString() + lastCoord.substring(1,2);
+                lastLastCoord = lastCoord;
+                /* if(lastLastCoord.substring(0,numeroValoresVerificarX) === '-1'){
+                    numeroValoresVerificarX = 1;
+                } */
+
+                partOne = (parseInt(lastCoord.substring(0,numeroValoresVerificarX),10) + 1).toString();
+                if(partOne == 0){
+                    numeroValoresVerificarX = 1;
+                }
+                partTwo = lastCoord.substring(numeroValoresVerificarX,numeroValoresVerificarX + numeroValoresVerificarY);
+                lastCoord = partOne + partTwo;
+                console.log("> " + lastCoord + "(" + lastCoord.substring(0,numeroValoresVerificarX) + " " + lastCoord.substring(numeroValoresVerificarX,numeroValoresVerificarX + numeroValoresVerificarY) +")//" + "nX: " + numeroValoresVerificarX + " ,nY: " + numeroValoresVerificarY);
+
+                if(lastCoord.substring(0,1) === '-'){//If Y coord goes negative
+                    if(lastCoord.substring(0,numeroValoresVerificarX) === '-'){//If X coord < 0
+                        numeroValoresVerificarX = 2;
+                    }
+                }else{//if its positive
+                    if(lastCoord.substring(0,numeroValoresVerificarX) > 9){//If Y coord passes 10
+                        numeroValoresVerificarX = 2;
+                    }
+                }
+
                 if(allMovesDict[lastCoord] === undefined){//If this is not verified, the outcome will be NaN(not a number)
                     allMovesDict[lastCoord] = 1;
                 }else{
@@ -35,7 +69,20 @@ const zeroThree = function(input){
                 }
             break;
             case 'v':
-                lastCoord = lastCoord.substring(0,1) + (parseInt(lastCoord.substring(1,2),10) - 1).toString();
+                lastLastCoord = lastCoord;
+                lastCoord = lastCoord.substring(0,numeroValoresVerificarX) + (parseInt(lastCoord.substring(numeroValoresVerificarX,numeroValoresVerificarX + numeroValoresVerificarY),10) - 1).toString();
+                console.log("v " + lastCoord + "(" + lastCoord.substring(0,numeroValoresVerificarX) + " " +lastCoord.substring(numeroValoresVerificarX,numeroValoresVerificarX + numeroValoresVerificarY) +")//" + "nX: " + numeroValoresVerificarX + " ,nY: " + numeroValoresVerificarY);
+
+                if(lastCoord.substring(numeroValoresVerificarX,numeroValoresVerificarX + 1) === '-'){//If Y coord goes negative
+                    if(lastCoord.substring(numeroValoresVerificarX,numeroValoresVerificarX + numeroValoresVerificarY) < -9){//If Y coord passes 10 negatives
+                        numeroValoresVerificarY = 3;
+                    }
+                }else{//if its positive
+                    if((lastCoord.substring(numeroValoresVerificarX,numeroValoresVerificarX + numeroValoresVerificarY),10) > 9){//If Y coord passes 10
+                        numeroValoresVerificarY = 2;
+                    }
+                }
+
                 if(allMovesDict[lastCoord] === undefined){
                     allMovesDict[lastCoord] = 1;
                 }else{
@@ -43,7 +90,21 @@ const zeroThree = function(input){
                 }
             break;
             case '<':
-                lastCoord = (parseInt(lastCoord.substring(0,1),10) - 1).toString() + lastCoord.substring(1,2);
+                lastLastCoord = lastCoord;
+                lastCoord = (parseInt(lastCoord.substring(0,numeroValoresVerificarX),10) - 1).toString() + lastCoord.substring(numeroValoresVerificarX,numeroValoresVerificarX + numeroValoresVerificarY);
+                console.log("< " + lastCoord + "(" + lastCoord.substring(0,numeroValoresVerificarX) + " " +lastCoord.substring(numeroValoresVerificarX,numeroValoresVerificarX + numeroValoresVerificarY) +")//" + "nX: " + numeroValoresVerificarX + " ,nY: " + numeroValoresVerificarY);
+                
+                if(lastCoord.substring(0,1) === '-'){//If X coord goes negative
+                    if(lastCoord.substring(0,numeroValoresVerificarX) === '-'){//If X coord < 0
+                        numeroValoresVerificarX = 2;
+                    }
+                }else{//if its positive
+
+                    if(lastCoord.substring(0,numeroValoresVerificarX) > 9){//If X coord passes 10
+                        numeroValoresVerificarX = 2;
+                    }
+                }
+
                 if(allMovesDict[lastCoord] === undefined){
                     allMovesDict[lastCoord] = 1;
                 }else{
@@ -51,10 +112,11 @@ const zeroThree = function(input){
                 }
             break;
         }
+
         totalPresents++;
     }
     
-    console.log(allMovesDict)
+    //console.log(allMovesDict)
     for(var key in allMovesDict){
         var value = allMovesDict[key];
         if(allMovesDict[key] > 1){
